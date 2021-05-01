9acf79169daba444b5ead115148a4a4f10367212
Argha Sarkar <argha.sarkar1994@gmail.com>
2021-05-02 00:17:34 +0100

    WIP

--- src/VisWax.py
+++ src/VisWax.py
@@ -18,24 +18,41 @@ class VisWax:
         # headers = {
         #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
         # }
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.31 SmartTV/6.0"
-        }
         # headers = {
-        #     # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        #     # "Accept-Encoding": "gzip, deflate, br",
+        #     "User-Agent": "Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.31 SmartTV/6.0"
+        # }
+        # headers = {
+        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        #     "Accept-Encoding": "gzip",
         #     # "Cache - Control": "max - age = 0",
-        #     # "Connection": "keep-alive",
+        #     "Connection": "keep-alive",
         #     # "Accept-Language": "en-US,en;q=0.9",
         #     # "DNT": "1",
-        #     # "Host": "secure.runescape.com",
-        #     # "Upgrade-Insecure-Requests": "1",
-        #     # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+        #     # "Host": "compute.amazonaws.com",
+        #     "Upgrade-Insecure-Requests": "1",
+        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
         # }
 
-        # headers = {
-        #
-        # }
+        headers = {
+            "Accept": "text / html, application / xhtml + xml, application / xml;q = 0.9, image / avif, image / webp, image / apng, * / *;q = 0.8, application / signed - exchange;v = b3;q = 0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en - US, en;q = 0.9",
+            "Cache-Control": "max-age=0",
+            "Connection": "keep - alive",
+
+            #
+            "DNT": "1",
+            "Host": "secure.runescape.com",
+            "sec-ch-ua": "Not A;Brand;v = 99,Chromium;v = 90, Google Chrome;v = 90",
+            "sec-ch-ua-mobile":"?0",
+            "Sec-Fetch-Dest":"document",
+            "Sec-Fetch-Mode":"navigate",
+            "Sec-Fetch-Site":"none",
+            "Sec-Fetch-User":"?1",
+            "Upgrade-Insecure-Requests":"1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
+
+        }
 
         print("A002: Fetching webpage")
         resp = requests.get(self.URL, headers=headers)
@@ -119,3 +136,8 @@ class VisWax:
 
         print("A012: Error during speechify")
         return self.result["error"]
+
+
+#
+v = VisWax()
+viswax_combo_result = v.get_viswax_combo(True)
