2dc0e2ba776d218cdf3e1bd37931b7e17af6905f
Argha Sarkar <argha.sarkar1994@gmail.com>
2021-05-02 02:21:10 +0100

    WIP

--- src/script.py
+++ src/script.py
@@ -1,12 +1,17 @@
 import scrapy
+#
 
+# print(scrapy)
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
     start_urls = ['https://secure.runescape.com/m=forum/forums?75,76,331,66006366']
 
     def parse(self, response):
-        for title in response.css('.oxy-post-title'):
+        print("Response:")
+        print(response.css)
+        for title in response.css('.forum-post__body'):
             yield {'title': title.css('::text').get()}
 
         for next_page in response.css('a.next'):
             yield response.follow(next_page, self.  parse)
+
